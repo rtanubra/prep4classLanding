c10f209ce19c5a5acb88b50c21256aef3062aea4
rtanubra <rdtanubrata@gmail.com>
2020-01-08 07:28:50 -0500

    arrow not ready for deployment

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 $( document ).ready(function() {
-    $(".navigate_up").hide()
+
     $(window).scroll(function() {
-        if($(window).scrollTop() > $(window).height()*.06){
+        if($(window).scrollTop() > $(document).height()*.06){
             $(".navigate_up").show()
         }else{
             $(".navigate_up").hide()
+            console.log('hide')
         }
     });
 })
